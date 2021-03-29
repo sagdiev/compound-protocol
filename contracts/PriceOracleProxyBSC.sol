6b17dc22aac585a2a97585df79b3622d82e832b1
bun919tw <bun919tw@gmail.com>
2021-03-29 15:33:25 +0800

    contracts/PriceOracleProxyBSC.sol: update BSC price oralce

--- contracts/PriceOracleProxyBSC.sol
+++ contracts/PriceOracleProxyBSC.sol
@@ -32,18 +32,108 @@ interface IStdReference {
         returns (ReferenceData[] memory);
 }
 
-contract PriceOracleProxyBSC is PriceOracle, Exponential {
-    /**
-     * @dev Possible error codes that we can return for BAND oracle
-     */
-    enum OracleError {
-        NO_ERROR,
-        ERR_INVALID,
-        ERR_REVERTED
-    }
+// Ref: https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Pair.sol
+interface IUniswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
 
-    /// @notice Admin address that could set the underlying symbol of crTokens
+    function name() external pure returns (string memory);
+
+    function symbol() external pure returns (string memory);
+
+    function decimals() external pure returns (uint8);
+
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address owner) external view returns (uint);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint value
+    ) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    function nonces(address owner) external view returns (uint);
+
+    function permit(
+        address owner,
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+
+    function factory() external view returns (address);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
+    function price0CumulativeLast() external view returns (uint);
+
+    function price1CumulativeLast() external view returns (uint);
+
+    function kLast() external view returns (uint);
+
+    function mint(address to) external returns (uint liquidity);
+
+    function burn(address to) external returns (uint amount0, uint amount1);
+
+    function swap(
+        uint amount0Out,
+        uint amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+
+    function skim(address to) external;
+
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+contract PriceOracleProxyBSC is PriceOracle, Exponential {
+    /// @notice Admin address
     address public admin;
+
+    /// @notice Guardian address
+    address public guardian;
 
     /// @notice Indicator that this is a PriceOracle contract (for inspection)
     bool public constant isPriceOracle = true;
@@ -57,12 +147,18 @@ contract PriceOracleProxyBSC is PriceOracle, Exponential {
     /// @notice The BAND oracle contract
     IStdReference public ref;
 
-    /// @notice The mapping records the crToken and its underlying symbol that we use for BAND reference
-    ///         It's not necessarily equals to the symbol in the underlying contract
-    mapping(address => string) public underlyingSymbols;
+    /// @notice The mapping records the token address and its symbol that is used by BAND reference
+    ///         It's not necessarily equals to the token symbol defined in their contract.
+    ///         For example, we use symbol BTC for BTCB when it comes to BAND refernce, not BTCB.
+    mapping(address => string) public symbols;
+
+    /// @notice Check if the underlying address is Pancakeswap LP
+    mapping(address => bool) public areUnderlyingLPs;
 
     /// @notice crBNB address that has a constant price of 1e18
     address public cBnbAddress;
+
+    address public constant wbnbAddress = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
 
     /**
      * @param admin_ The address of admin to set underlying symbols for BAND oracle
@@ -93,63 +189,118 @@ contract PriceOracleProxyBSC is PriceOracle, Exponential {
             return 1e18;
         }
 
-        bytes memory symbol = bytes(underlyingSymbols[cTokenAddress]);
-        if (symbol.length != 0) {
-            OracleError oracleErr;
-            Exp memory price;
-            (oracleErr, price) = getPriceFromBAND(string(symbol));
-            if (oracleErr != OracleError.NO_ERROR) {
-                // Fallback to v1 PriceOracle
-                return getPriceFromV1(cTokenAddress);
-            }
-
-            MathError mathErr;
-            uint underlyingDecimals;
-            underlyingDecimals = BEP20Interface(CErc20(cTokenAddress).underlying()).decimals();
-            (mathErr, price) = mulScalar(price, 10**(18 - underlyingDecimals));
-            if (mathErr != MathError.NO_ERROR) {
-                // Fallback to v1 PriceOracle
-                return getPriceFromV1(cTokenAddress);
-            }
-
-            return price.mantissa;
-        }
-
-        return getPriceFromV1(cTokenAddress);
-    }
-
-    function getPriceFromBAND(string memory symbol) internal view returns (OracleError, Exp memory) {
-        (bool success, bytes memory returnData) =
-            address(ref).staticcall(
-                abi.encodePacked(
-                    ref.getReferenceData.selector,
-                    abi.encode(symbol, QUOTE_SYMBOL)
-                )
-            );
-        if (success) {
-            IStdReference.ReferenceData memory data = abi.decode(returnData, (IStdReference.ReferenceData));
-            if (data.rate == 0) {
-                return (OracleError.ERR_INVALID, Exp({mantissa: 0}));
-            }
-            return (OracleError.NO_ERROR, Exp({mantissa: data.rate}));
-        }
-        return (OracleError.ERR_REVERTED, Exp({mantissa: 0}));
-    }
-
-    function getPriceFromV1(address cTokenAddress) internal view returns (uint) {
         address underlying = CErc20(cTokenAddress).underlying();
-        return v1PriceOracle.assetPrices(underlying);
+        if (areUnderlyingLPs[cTokenAddress]) {
+            return getLPFairPrice(underlying);
+        }
+
+        return getTokenPrice(underlying);
     }
 
+    /*** Internal fucntions ***/
+
+    /**
+     * @notice Get the price of a specific token.
+     * @param token The token to get the price of
+     * @return The price
+     */
+    function getTokenPrice(address token) internal view returns (uint) {
+        if (token == wbnbAddress) {
+            // wbnb always worth 1
+            return 1e18;
+        }
+
+        bytes memory symbol = bytes(symbols[token]);
+        if (symbol.length != 0) {
+            IStdReference.ReferenceData memory data = ref.getReferenceData(string(symbol), QUOTE_SYMBOL);
+            uint underlyingDecimals = BEP20Interface(token).decimals();
+            return mul_(data.rate, 10**(18 - underlyingDecimals));
+        }
+        return getPriceFromV1(token);
+    }
+
+    /**
+     * @notice Get the fair price of a LP. We use the mechanism from Alpha Finance.
+     *         Ref: https://blog.alphafinance.io/fair-lp-token-pricing/
+     * @param pair The pair of AMM (Pancakeswap)
+     * @return The price
+     */
+    function getLPFairPrice(address pair) internal view returns (uint) {
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
+        (uint r0, uint r1, ) = IUniswapV2Pair(pair).getReserves();
+        uint sqrtR = sqrt(mul_(r0, r1));
+        uint p0 = getTokenPrice(token0);
+        uint p1 = getTokenPrice(token1);
+        uint sqrtP = sqrt(mul_(p0, p1));
+        return div_(mul_(2, mul_(sqrtR, sqrtP)), totalSupply);
+    }
+
+    /**
+     * @notice Get price from v1 price oracle
+     * @param token The token to get the price of
+     * @return The price
+     */
+    function getPriceFromV1(address token) internal view returns (uint) {
+        return v1PriceOracle.assetPrices(token);
+    }
+
+    /*** Admin functions ***/
+
+    event SetAdmin(address admin);
+    event SetGuardian(address guardian);
+    event IsLPUpdated(address tokenAddress, bool isLP);
+    event SymbolUpdated(address tokenAddress, string symbol);
+
+    /**
+     * @notice Set admin for price oracle proxy
+     * @param _admin The new admin
+     */
     function _setAdmin(address _admin) external {
-        require(msg.sender == admin, "!admin");
+        require(msg.sender == admin, "only the admin may set new admin");
         admin = _admin;
+        emit SetAdmin(admin);
     }
 
-    function _setUnderlyingSymbols(address[] calldata cTokenAddresses, string[] calldata symbols) external {
-        require(msg.sender == admin, "!admin");
-        for (uint i = 0; i < cTokenAddresses.length; i++) {
-            underlyingSymbols[cTokenAddresses[i]] = symbols[i];
+    /**
+     * @notice Set guardian for price oracle proxy
+     * @param _guardian The new guardian
+     */
+    function _setGuardian(address _guardian) external {
+        require(msg.sender == admin, "only the admin may set new guardian");
+        guardian = _guardian;
+        emit SetGuardian(guardian);
+    }
+
+    /**
+     * @notice Set if a list of cToken are Pancakeswap LP or not.
+     * @param _cTokenAddresses The list of cToken address
+     * @param _isLP They are LPs or not
+     */
+    function _setLPs(address[] calldata _cTokenAddresses, bool[] calldata _isLP) external {
+        require(msg.sender == admin, "only the admin may set LPs");
+        require(_cTokenAddresses.length == _isLP.length, "mismatched data");
+        for (uint i = 0; i < _cTokenAddresses.length; i++) {
+            areUnderlyingLPs[_cTokenAddresses[i]] = _isLP[i];
+            emit IsLPUpdated(_cTokenAddresses[i], _isLP[i]);
+        }
+    }
+
+    /**
+     * @notice Set the token's symbol for BAND reference. If the symbol of a token is set, the price oracle will get its price from BAND protocol.
+     * @param _tokenAddresses The list of token address
+     * @param _symbols The list of symbols for BAND reference
+     */
+    function _setSymbols(address[] calldata _tokenAddresses, string[] calldata _symbols) external {
+        require(msg.sender == admin || msg.sender == guardian, "only the admin or guardian may set symbols");
+        require(_tokenAddresses.length == _symbols.length, "mismatched data");
+        for (uint i = 0; i < _tokenAddresses.length; i++) {
+            if (bytes(_symbols[i]).length != 0) {
+                require(msg.sender == admin, "guardian may only clear the symbol");
+            }
+            symbols[_tokenAddresses[i]] = _symbols[i];
+            emit SymbolUpdated(_tokenAddresses[i], _symbols[i]);
         }
     }
 }
